079aac8f45172e5e3054188b2b95ef323f47b34d
Five <co22rkn@protonmail.com>
2025-02-21 15:43:59 +0100

    started to implement git issues

--- src/app/components/GitActions.svelte
+++ src/app/components/GitActions.svelte
@@ -1,23 +1,27 @@
 <script lang="ts">
-  import {type TrustedEvent} from "@welshman/util"
+  import { load } from "@welshman/app"
+  import {Address, GIT_ISSUE, type Filter, type TrustedEvent} from "@welshman/util"
   import {pubkey} from "@welshman/app"
   import ReactionSummary from "@app/components/ReactionSummary.svelte"
   import ThunkStatusOrDeleted from "@app/components/ThunkStatusOrDeleted.svelte"
-  import EventActivity from "@app/components/EventActivity.svelte"
   import EventActions from "@app/components/EventActions.svelte"
   import {publishDelete, publishReaction} from "@app/commands"
   import {makeGitPath} from "@app/routes"
   import Button from "@src/lib/components/Button.svelte"
   import Link from "@src/lib/components/Link.svelte"
   import { nthEq } from "@welshman/lib"
+  import { onMount } from "svelte"
 
   interface Props {
     url: any
-    event: any
+    event: TrustedEvent
     showActivity?: boolean
   }
 
   const {url, event, showActivity}: Props = $props()
+  let issues: TrustedEvent[] = []
+  const issueFilter:Filter = {kinds: [GIT_ISSUE]}
+  let issueCount = $state(0)
 
   const path = makeGitPath(url, event.id)
 
@@ -30,21 +34,42 @@
     }
   }
 
+  const loadIssues =  async () => {
+    const address = Address.fromEvent(event)
+    issueFilter['#a'] = [address.toString()]
+    const [tagId, ...relays] = event.tags.find(nthEq(0, "relays")) || []
+    console.log("Issue Filter", issueFilter)
+    issues = await load({
+      relays: relays,
+      filters: [ issueFilter ]
+    })
+    console.log("Issues loaded:", issues)
+    issueCount = issues.length
+  } 
+
+  onMount(() => {
+    loadIssues()
+  })
+
   const web = event.tags.find(nthEq(0, "web"))?.[1]
 </script>
 
 <div class="flex flex-wrap items-center justify-between gap-2">
   <div class="flex flex-grow flex-wrap justify-end gap-2">
-      <Button class="btn btn-primary btn-sm">
-        <Link external class="w-full cursor-pointer" href={web}>
-          <span class="">Browse</span>
-        </Link>
-      </Button>
+    <Button class="btn btn-primary btn-sm">
+      <Link external class="w-full cursor-pointer" href={web}>
+        <span class="">Browse</span>
+      </Link>
+    </Button>
+    <Button class="btn btn-secondary btn-sm">
+      <Link class="w-full cursor-pointer" href={web}>
+        <span class="">{"Issues(" + issueCount + ")"}</span>
+      </Link>
+    </Button>
     {#if showActivity}
       <ReactionSummary {url} {event} {onReactionClick} reactionClass="tooltip-left" />
       <ThunkStatusOrDeleted {event} />
-      <EventActivity {url} {path} {event} />
-      <EventActions {url} {event} noun="Job" />
+      <EventActions {url} {event} noun="Repo" />
     {/if}
   </div>
 </div>
